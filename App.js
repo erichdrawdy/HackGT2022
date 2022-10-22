36dc1b7bafc24b44df3b5e88e5d6d7fac44a0a03
Tomer Shmul <45541706+ShmulTomer@users.noreply.github.com>
2022-10-22 02:15:41 -0400

    New pages

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//This is an example code for Navigator// 
+import React, { Component } from 'react';
+//import react in our code. 
+ 
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+ 
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
+import ThirdPage from './pages/ThirdPage';
+//import all the screens we are going to switch 
+const App = createStackNavigator({
+  //Constant which holds all the screens like index of any book 
+    FirstPage: { screen: FirstPage }, 
+    //First entry by default be our first screen if we do not define initialRouteName
+    SecondPage: { screen: SecondPage }, 
+    ThirdPage: { screen: ThirdPage }, 
   },
-});
+  {
+    initialRouteName: 'FirstPage',
+  }
+);
+export default createAppContainer(App);
